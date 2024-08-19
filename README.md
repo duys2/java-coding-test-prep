288a6ecdbde058cd7c1ea4f3315447fb15c38e9b
duys2 <minigun5@naver.com>
2024-08-19 22:09:26 +0900

    [Docs] README.md Update

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Java 코딩 테스트 준비
 ## 프로그래머스 문제
+### Discord Webhook
---
+++ src/question/Solution16.java
@@ -0,0 +1,24 @@
+package question;
+
+public class Solution16 {
+	/* 문제 설명
+	정수 num과 n이 매개 변수로 주어질 때,
+	num이 n의 배수이면 1을 return n의 배수가 아니라면 0을 return 하도록 solution 함수를 완성해 주세요.
+	*/
+
+	/* 제한사항
+	2 ≤ num ≤ 100
+	2 ≤ n ≤ 9
+	*/
+
+	public static void main(String[] args) {
+
+	}
+
+	public int solution(int num, int n) {
+		int answer = 0;
+		return answer;
+
+		/* --------------- 다른 사람 풀이 --------------- */
+	}
+}
