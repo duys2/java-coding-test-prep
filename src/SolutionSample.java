9c47dc2e05309b449d654a0789b8b688983e63e7
duys2 <minigun5@naver.com>
2024-08-19 21:45:18 +0900

    [Add] Sample code add

--- src/SolutionSample.java
+++ src/SolutionSample.java
@@ -1,13 +1,14 @@
 public class SolutionSample {
 	/* 문제 설명
 
-	*/
+	 */
 
 	/* 제한사항
 
-	*/
+	 */
 
 	public static void main(String[] args) {
+		/* --------------- 다른 사람 풀이 --------------- */
 
 	}
 }
