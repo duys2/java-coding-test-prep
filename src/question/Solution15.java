9667f4dc3ef1dc446ae76fa141f31e05a6b89ff2
duys2 <minigun5@naver.com>
2024-08-19 21:44:27 +0900

    [Feat] 15번째 문제

--- src/question/Solution15.java
+++ src/question/Solution15.java
@@ -6,9 +6,9 @@ public class Solution15 {
 
 	12 ⊕ 3 = 123
 	3 ⊕ 12 = 312
-	양의 정수 a와 b가 주어졌을 때, a ⊕ b와 b ⊕ a 중 더 큰 값을 return 하는 solution 함수를 완성해 주세요.
+	양의 정수 a와 b가 주어졌을 때, a ⊕ b와 2 * a * b 중 더 큰 값을 return하는 solution 함수를 완성해 주세요.
 
-	단, a ⊕ b와 b ⊕ a가 같다면 a ⊕ b를 return 합니다.
+	단, a ⊕ b와 2 * a * b가 같으면 a ⊕ b를 return 합니다.
 	*/
 
 	/* 제한사항
@@ -16,21 +16,13 @@ public class Solution15 {
 	*/
 
 	public static void main(String[] args) {
-		System.out.println(new Solution15().solution(4, 81));
+		System.out.println(new Solution15().solution(2, 91));
 	}
 
 	public int solution(int a, int b) {
-		int answer = 0;
-
-		String ab = String.valueOf(a) + String.valueOf(b);
-		String ba = String.valueOf(b) + String.valueOf(a);
-
-		answer = Integer.parseInt(ab) < Integer.parseInt(ba) ? Integer.parseInt(ba) : Integer.parseInt(ab);
-		// answer = Math.max(Integer.parseInt(ab), Integer.parseInt(ba));
-
-		return answer;
+		return Math.max(Integer.parseInt("" + a + b), (2 * a * b));
 
 		/* --------------- 다른 사람 풀이 --------------- */
-		// return Math.max(Integer.parseInt(a + "" + b), Integer.parseInt(b + "" + a));
+		// return Math.max(Integer.parseInt(String.valueOf(a) + String.valueOf(b)), 2 * a * b);
 	}
 }
